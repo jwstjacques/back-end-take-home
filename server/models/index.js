4f2528b1f02962a93b3aba8babd5bd9b5a359b80
Jason St. Jacques <jw.stjacques@gmail.com>
2019-08-22 08:43:08 -0400

    Resetting models/index to working state

--- server/models/index.js
+++ server/models/index.js
@@ -2,7 +2,6 @@
 
 const fs = require('fs');
 const path = require('path');
-const http = require('http');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
@@ -11,13 +10,7 @@ const db = {};
 
 let sequelize;
 if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], {
-    dialect: 'postgres',
-    protocol: 'postgres',
-    port: match[4],
-    host: match[3],
-    logging: true //false
-  });
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
   sequelize = new Sequelize(
     config.database,
